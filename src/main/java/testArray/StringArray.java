3019095fe832fe250933af8e2e4d0ee8b09b0a36
Gleb <Glebas3@gmail.com>
2017-07-06 23:47:26 +0300

    Test 0.2

---
+++ src/main/java/testArray/StringArray.java
@@ -0,0 +1,42 @@
+package testArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Gleb on 06.07.17.
+ */
+public class StringArray {
+
+    private String text;
+
+    public StringArray(String text) {
+        this.text = text;
+    }
+
+    public Map<String, Integer> wordsCount() {
+        Map<String, Integer> hashMap = new HashMap<>();
+        String[] words = text.split(" ");
+        for (int i = 0 ; i < words.length; i++) {
+            if (!hashMap.containsKey(words[i])) {
+                  hashMap.put(words[i], 1);
+            } else hashMap.put(words[i], hashMap.get(words[i]) + 1);
+        }
+        return  hashMap;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        String text = "Fast access to MySQL software Downloads" +
+                "Download technical White Papers and Presentations" +
+                "Post messages in the MySQL Discussion Forums" +
+                "Report and track bugs in the MySQL bug system" +
+                "Comment in the MySQL Documentation";
+
+        System.out.println(new StringArray(text).wordsCount());
+    }
+
+
+}
