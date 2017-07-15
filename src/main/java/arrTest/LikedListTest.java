eb44105587f08497e6c13137662349329ff857b7
Gleb <Glebas3@gmail.com>
2017-07-15 13:06:19 +0300

    Test 0.2

--- src/main/java/arrTest/LikedListTest.java
+++ src/main/java/arrTest/LikedListTest.java
@@ -1,11 +1,166 @@
 package arrTest;
 
+import java.util.*;
+
 /**
  * Created by Gleb on 24.06.17.
  */
-public class LikedListTest<E> {
-  transient int size = 0;
+public class LikedListTest<E> implements List {
+    private int size = 0;
+
+    private Node<E> first;
+    private Node<E> last;
+
+
+    public static class Node<E> {
+
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.item = element;
+            this.next = next;
+        }
 
 
 
+    }
+
+    public void likdFirst(E e){
+        Node<E> first = this.first;
+        Node<E> newNode = new Node<>(null, e, first);
+        if(first == null)
+                this.last = newNode;
+            else
+                first.prev = newNode;
+          size++;
+    }
+
+    public void likdLast(E e){
+        Node<E> last = this.last;
+        Node<E> newNode = new Node<>(last, e, null);
+        if(last == null)
+            this.first = newNode;
+        else
+            first.next = newNode;
+        size++;
+    }
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
 }
